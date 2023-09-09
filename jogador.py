13a740aba2ddeaa1fa650d6f4447dc8717bab54d
jota-f <jordano.f@gmail.com>
2023-09-09 00:27:23 -0300

    Create jogador.py

---
+++ jogador.py
@@ -0,0 +1,10 @@
+from webscraping import obter_carreira_do_jogador
+
+def comparar_carreiras(jogador1, jogador2):
+    # (Função existente)
+
+if __name__ == "__main__":
+    jogador1 = input("Digite o nome do primeiro jogador: ")
+    jogador2 = input("Digite o nome do segundo jogador: ")
+
+    comparar_carreiras(jogador1, jogador2)
